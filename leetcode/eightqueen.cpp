00a859e2474f4eeafe2273532a1ba6c756be444f
sherlockjin <jinxiaolekk@163.com>
2017-08-25 16:35:54 +0800

    111

--- leetcode/eightqueen.cpp
+++ leetcode/eightqueen.cpp
@@ -1,21 +1,21 @@
 #include"common.h"
 
 void eightqueen(int i);
-const int size = 4;
-char queen[size][size];//存放棋盘
-bool a[size], b[size + size - 1], c[size + size - 1];//a为列冲突，b为主对象线冲突（右上角为0，为i-j+(size-1)），c为从对象线（左上角为0,（i+j)）
+const int sizew = 4;
+char queen[sizew][sizew];//存放棋盘
+bool a[sizew], b[sizew + sizew - 1], c[sizew + sizew - 1];//a为列冲突，b为主对象线冲突（右上角为0，为i-j+(sizew-1)），c为从对象线（左上角为0,（i+j)）
 int queenNum = 0;
 
 //int main(){
-//	for (int i = 0; i < size; i++){
-//		for (int j = 0; j < size; j++){
+//	for (int i = 0; i < sizew; i++){
+//		for (int j = 0; j < sizew; j++){
 //			queen[i][j] = '*';
 //		}
 //	}
-//	for (int i = 0; i < size; i++){
+//	for (int i = 0; i < sizew; i++){
 //		a[i] = false;
 //	}
-//	for (int i = 0; i < size + size - 1; i++){
+//	for (int i = 0; i < sizew + sizew - 1; i++){
 //		b[i] = false;
 //		c[i] = false;
 //	}
@@ -26,22 +26,22 @@ int queenNum = 0;
 
 void eightqueen(int i){
 
-	if (i >= size){
+	if (i >= sizew){
 		return;
 	}
-	for (int j = 0; j < size; j++){
-		if (!a[j] && !b[i - j + size-1] && !c[i + j]){//没有冲突
+	for (int j = 0; j < sizew; j++){
+		if (!a[j] && !b[i - j + sizew-1] && !c[i + j]){//没有冲突
 			queen[i][j] = '@';
 			a[j] = true;
-			b[i - j + size-1] = true;
+			b[i - j + sizew-1] = true;
 			c[i + j] = true;
-			if (i < size - 1){
+			if (i < sizew - 1){
 				eightqueen(i + 1);
 			}
 			else{
 				queenNum++;
-				for (int i = 0; i < size; i++){
-					for (int j = 0; j < size; j++){
+				for (int i = 0; i < sizew; i++){
+					for (int j = 0; j < sizew; j++){
 						cout << queen[i][j] << " ";
 					}
 					cout << endl;
@@ -56,7 +56,7 @@ void eightqueen(int i){
 			*/
 			queen[i][j] = '*';
 			a[j] = false;
-			b[i - j + size-1] = false;
+			b[i - j + sizew-1] = false;
 			c[i + j] = false;
 			
 		}
