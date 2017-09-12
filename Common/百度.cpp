28ea4674e442c0a55ebf84df180fa8eeacb356ea
sherlockjin <jinxiaolekkk@163.com>
2017-09-12 18:07:06 +0800

    ~

--- Common/百度.cpp
+++ Common/百度.cpp
@@ -3,11 +3,32 @@ using namespace std;
 
 class Baidu{
 public:
+	int zhixian(int num, int timX, int timY, int** coordinates)
+	{
+		int res = 0;
+		map<double, int> k;
+		for (int i = 0; i < num; i++)
+		{
+			int tempX = coordinates[i][0] - timX;
+			int tempY = coordinates[i][1] - timY;
+			if (tempX != 0)
+			{
+				double tempK = (double)tempY / tempX;
+				k[tempK] = 1;
+			}
+			else
+			{
+				double tempK = 0.0;
+				k[tempK] = 1;
+			}
+			return k.size();
+
+		}
+	}
 
 };
 
 int main()
 {
-	cout << "hello, baidu";
-	cout << endl;
+	
 }
--- didi/subMaxSum.cpp
+++ didi/subMaxSum.cpp
@@ -4,7 +4,7 @@ const int MAX_N = 1e5 + 5;
 
 
 class SubMaxSum{
-
+public:
 	int  a[MAX_N];
 	int test() {
 
@@ -14,18 +14,32 @@ class SubMaxSum{
 		int res, last;
 		res = -2147483647 - 1;
 		last = 0;
+		int begin = 0, end = 0,gbegin = 0, gend = 0;
 		for (int i = 0; i < n; i++) {
 			if (last > 0) {
 				last += a[i];
 			}
 			else {
+				begin = i;
 				last = a[i];
 			}
-			res = max(res, last);
+			end = i;
+			if (last > res){
+				gbegin = begin;
+				gend = end;
+				res = last;
+			}
+			
 		}
 		printf("%d\n", res);
 		return 0;
 
 	}
 };
+
+//int main()
+//{
+//	SubMaxSum sms;
+//	sms.test();
+//}
 
