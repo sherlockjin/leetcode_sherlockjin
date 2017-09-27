39680859fd5915c2c43fe2e63f43f8b6b1a4df62
sherlockjin <jinxiaolekkk@163.com>
2017-09-27 18:49:22 +0800

    11

--- Common/语法.cpp
+++ Common/语法.cpp
@@ -1,159 +1,86 @@
-#include"common.h"
-#include<assert.h>
-class CExample
-{
+#include <iostream>
+#include <string.h>
+using namespace std;
 
-	
+
+//拷贝构造函数 和构造函数
+class A
+{
+private:
+	int* n;
+
 public:
-	int a;
-	//构造函数
-	CExample(){
-		cout << "default constructor"  << endl;
+	A()
+	{
+		n = new int[10];
+		n[0] = 1;
+		cout << "constructor is called\n";
 	}
-	CExample(int b)
+
+	A(const A& a) //拷贝构造函数的参数一定是引用，不能不是引用，不然会出现无限递归
 	{
-		a = b;
-		cout << "constructor is called: " << a << endl;
+		n = new int[10];
+		memcpy(n, a.n, 10);  //通过按字节拷贝，将堆中一块内存存储到另一块内存
+		cout << "copy constructor is called\n";
 	}
-	//拷贝构造函数
-	CExample(const CExample& A)
+
+	A operator=(const A& a)   //传参和返回值设置错误
 	{
-		a = A.a;
-		cout << "copy constructor is called: " << a << endl;
-		
-	}
-	/*CExample operator=(const CExample& A)
-	{
-		CExample B(A);
-		printf("fuzhi is called\n");
-		return B;
-	}*/
-
-	//析构函数
-	~CExample()
-	{
-		cout << "destructor is called: " << a << endl;
-	}
-	void Show()
-	{
-		cout << a << endl;
-	}
-};
-
-//防止按值传递
-//class CExample
-//{
-//private:
-//	int a;
-//
-//public:
-//	//构造函数
-//	CExample(int b)
-//	{
-//		a = b;
-//		cout << "creat: " << a << endl;
-//	}
-//
-//private:
-//	//拷贝构造函数，只是声明
-//	CExample(const CExample& C);
-//
-//public:
-//	~CExample()
-//	{
-//		cout << "delete: " << a << endl;
-//	}
-//
-//	void Show()
-//	{
-//		cout << a << endl;
-//	}
-//};
-
-
-void arg_fun(CExample C)
-{
-	cout << "arg : CExample" << endl;
-}
-CExample ret_fun()
-{
-	CExample temp(19);
-	cout << "return : CExample" << endl;
-	return temp;
-}
-//int main()
-//{
-//	CExample A(1);
-//	//arg_fun(A);   //按值传递将出错
-//	ret_fun();
-//	A.Show();
-//	return 0;
-//}
-
-/*
-(1).A对象传入形参时，会先会产生一个临时变量，就叫 C 吧。
-(2).然后调用拷贝构造函数把A的值给C。 整个这两个步骤有点像：CExample C(A);
-(3).等g_fun()执行完后, 析构掉 C 对象。
-
-*/
-
-
-
-
-
-//int main()
-//{
-//	CExample A(100);
-//	arg_fun(A);
-//	ret_fun();
-//	A.Show();
-//	//B.Show();
-//	return 0;
-//}
-
-class Rect
-{
-public:
-	Rect()
-	{
-		p = new int[100];
-		for (int i = 0; i < 100; i++)
+		if (this == &a)
+			return *this;
+		if (n != NULL)
 		{
-			p[i] = i;
+			delete n;
+			n == NULL;
 		}
+
+		n = new int[10];
+		memcpy(n, a.n, 10);
+		cout << "assign constructor is called\n";
+		return *this;
 	}
-	Rect(const Rect& r)
+
+	~A()
 	{
-		width = r.width;
-		height = r.height;
-		p = new int[100];
-		*p = *(r.p);
-		/*for (int i = 0; i < 100; i++)
-		{
-			p[i] = r.p[i];
-		}*/
-
+		cout << "destructor is called\n";
+		delete n;
 	}
-	~Rect()
+
+	void get()
 	{
-		assert(p != NULL);
-		delete p;
+		cout << "n[0]: " << n[0] << endl;
 	}
+	
 
-
-	int width;
-	int height;
-	int *p;
 };
 
-
+void arg(A a)
+{
+	cout << "arg : A" << endl;
+}
+A ret()
+{
+	A a;
+	cout << "return : A" << endl;
+	return a;
+}
 //int main()
 //{
-//	Rect rect1;
-//	Rect rect2(rect1);
-//	for (int i = 0; i < 10; i++)
-//	{
-//		cout << rect2.p[i];
-//	}
+//	A a ;
+//	//------------------------
+//	 //A b(ret()); //ret返回的就是临时对象，直接b就是那个临时对象，不再调用拷贝构造函数或者赋值函数
+//	//---------------------------
+//	A b;
+//	b = a;
+//	//b = ret(); //ret()返回临时对象之后，进入赋值函数，
+//				//正常情况下赋值函数的参数和返回值都是引用，所以不再调用拷贝构造函数，返回后把临时对象析构
+//				//当参数不是引用，返回值是引用时，由于引用也是临时对象，故不再调用拷贝构造函数，与正常情况一样
+//				//当返回值不是引用时，在返回值需要调用拷贝构造函数，之后析构掉这个和临时对象
+//	b.get();
+//
 //	return 0;
 //}
+
+
+
+
